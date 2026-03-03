4ccad8f6cadc7cf25f0c04dc7b4f7c8e18829788
Florian LABADIE <florian.labadie@epitech.eu>
2026-03-03 09:53:27 +0100

    style: indentation externs

--- UIEngine.cpp
+++ UIEngine.cpp
@@ -4,10 +4,10 @@
 #include "imgui.h"
 
 extern "C++" {
-std::unique_ptr<ui::UIEngine> get_ui_engine()
-{
-    ImGui::DebugLog("Debug from ImGui");
-    std::cout << "Hello from ui engine" << std::endl;
-    return std::make_unique<ui::UIEngine>();
-}
+    std::unique_ptr<ui::UIEngine> get_ui_engine()
+    {
+        ImGui::DebugLog("Debug from ImGui");
+        std::cout << "Hello from ui engine" << std::endl;
+        return std::make_unique<ui::UIEngine>();
+    }
 }
