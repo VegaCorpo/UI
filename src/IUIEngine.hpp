0f1a311bcec52cd9b19218abf892f42a9cfa4f1c
baptistebd <baptiste.blambert@epitech.eu>
2026-03-05 15:05:20 +0100

    chore: add comments in IUIEngine

--- src/IUIEngine.hpp
+++ src/IUIEngine.hpp
@@ -4,11 +4,15 @@
 
 namespace ui {
     class IUIEngine {
+
         public:
+            // Destructor
             virtual ~IUIEngine() = default;
 
+            // Method to set Layer according the GUI you want to use
             virtual void setLayer(ILayer *layer) = 0;
 
+            // Update UI frame and convert it for the renderer
             virtual void update(float dt, float w, float h) = 0;
     };
 } // namespace ui
