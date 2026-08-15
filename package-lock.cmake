6c8478eeae9a024747bb7ec3bad315b35a52cc6e
Baptiste Blambert <146111727+baptistebd@users.noreply.github.com>
2026-08-15 20:20:09 +0200

    fix: cmake packages + display size based on render (#21)
    * fix: cmake packages + display size based on render
    
    * fix: wayland-scanner with GLFW package-log flag
    
    * feat: add X11 package in UI CI for compilation
    
    * feat: add libxrand package CI UI
    
    * feat: add all  package CI UI

--- package-lock.cmake
+++ package-lock.cmake
@@ -10,7 +10,21 @@ CPMDeclarePackage(ImGui
 #Common
 CPMDeclarePackage(Common
     GITHUB_REPOSITORY VegaCorpo/Common
-    GIT_TAG main
+    GIT_TAG v0.1.0
+    SYSTEM YES
+    EXCLUDE_FROM_ALL YES
+)
+
+# GLFW
+CPMDeclarePackage(glfw
+    GITHUB_REPOSITORY glfw/glfw
+    GIT_TAG 3.4
+    OPTIONS
+        "GLFW_BUILD_EXAMPLES OFF"
+        "GLFW_BUILD_TESTS OFF"
+        "GLFW_BUILD_DOCS OFF"
+        "BUILD_SHARED_LIBS ON"
+        "GLFW_BUILD_WAYLAND OFF"
     SYSTEM YES
     EXCLUDE_FROM_ALL YES
 )
